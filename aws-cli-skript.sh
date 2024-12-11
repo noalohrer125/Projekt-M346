953133f361ae7910a17244a0ee9cdc1ec875da5a
noaloehrer <noa.loehrer@optimatik.ch>
2024-12-11 14:43:55 +0100

    aws-cli-skript created

---
+++ aws-cli-skript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BUCKET_INPUT="csv-input-bucket"
+BUCKET_OUTPUT="json-output-bucket"
+
+# S3-Buckets erstellen
+aws s3api create-bucket --bucket $BUCKET_INPUT --region us-east-1
+aws s3api create-bucket --bucket $BUCKET_OUTPUT --region us-east-1
+
+# Lambda-Bereitstellung
+aws lambda create-function \
+    --function-name CsvToJson \
+    --runtime dotnet8 \
+    --role arn:aws:iam::123456789012:role/lambda-role \
+    --handler lambda_function.lambda_handler \
+    --code S3Bucket=Bucket_Input,S3Key=lambda.zip \
+    --timeout 15
+
+# Ereignisquelle hinzufügen
+aws s3api put-bucket-notification-configuration \
+    --bucket $BUCKET_INPUT \
+    --notification-configuration file://notification.json
